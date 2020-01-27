c9d266259e0d28ea16058cbc6dab5f79eb94edb6
Nishant Singh <saysnishant@gmail.com>
2020-01-28 00:23:21 +0530

    logo added, now.json added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import Head from 'next/head'
+
 function HomePage () {
   return (
     <div>
-      Vocarious
+      <Head>
+        <title>Vocarious</title>
+      </Head>
+      <img
+        style={{
+          position: 'fixed', 
+          top: 'calc(50vh - 25px)',
+          height: '50px',
+          width: 'auto',
+          left: 'calc(50vw - 80px)',
+        }}
+        src="/images/logo.png"
+      />
     </div>
   );
 }
